1ce4884d1dd92b7a09ce1ac19df5bc4fe0785fff
matheusnff85 <math.marinho154@gmail.com>
2022-10-21 17:24:04 -0300

    feat: middleware de erro

---
+++ src/errors/catalog.ts
@@ -0,0 +1,24 @@
+export enum ErrorTypes {
+  NotFound = 'NotFound',
+  InvalidId = 'InvalidId',
+}
+
+type ErrorResponseObject = {
+  message: string;
+  httpStatus: number;
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject
+};
+
+export const errorCatalog: ErrorCatalog = {
+  NotFound: {
+    message: 'Object not found',
+    httpStatus: 404,
+  },
+  InvalidId: {
+    message: 'Id must have 24 hexadecimal characters',
+    httpStatus: 400,
+  },
+};
