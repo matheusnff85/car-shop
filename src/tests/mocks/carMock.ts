7a5077525914ab85f690d24fff37f680b7e44794
matheusnff85 <math.marinho154@gmail.com>
2022-10-22 11:14:03 -0300

    feat: testes do model de carros

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -21,4 +21,25 @@ const carMockWithId:ICar & { _id: string } = {
   buyValue: 1500000,
 }
 
-export { carMock, carMockWithId };
+const carChangeMock:ICar = {
+  doorsQty: 4,
+  seatsQty: 4,
+  model: 'Ferrari',
+  year: 2020,
+  color: 'Azul',
+  status: true,
+  buyValue: 1800000,
+}
+
+const carChangeMockWithId:ICar & { _id: string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  doorsQty: 4,
+  seatsQty: 4,
+  model: 'Ferrari',
+  year: 2020,
+  color: 'Azul',
+  status: true,
+  buyValue: 1800000,
+}
+
+export { carMock, carMockWithId, carChangeMock, carChangeMockWithId };
