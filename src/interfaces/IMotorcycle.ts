62e02298d58bb3c74c15cb23a7640754f6570c2b
matheusnff85 <math.marinho154@gmail.com>
2022-10-23 13:41:13 -0300

    feat: motorcycle model

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -2,6 +2,12 @@ import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
 const motorcycleZodSchema = z.object({
+  _id: z.string().optional(),
+  model: z.string().min(3),
+  year: z.number().int().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
   category: z.enum(['Street', 'Custom', 'Trail']),
   engineCapacity: z.number().int().lte(2500),
 });
